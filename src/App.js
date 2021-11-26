180a974c5b5a7fcd5a14b0346fe05b501661f979
unknown <santhosh8857@gmail.com>
2021-11-27 03:11:05 +0530

    added Dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -8,7 +10,12 @@ function App() {
       <div id="wrapper">
         <Navbar />
         <div id="content-wrapper" class="d-flex flex-column">
-          <Header />
+          <div id="content">
+            <Header />
+            <div class="container-fluid">
+              <Dashboard />
+            </div>
+          </div>
         </div>
       </div>
     </>
